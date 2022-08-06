d4c5a150b564e4e0b80a5a5d49b0d3da5153f65b
Antoine Kingue <etorefr@gmail.com>
2022-08-06 18:13:17 +0200

    ✨ add room entity

---
+++ src/main/java/fr/antoinek/roomrental/entity/Rooms.java
@@ -0,0 +1,87 @@
+package fr.antoinek.roomrental.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-data-jpa-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-17
+ * Time: 05:47
+ * To change this template use File | Settings | File Templates.
+ */
+@Document
+public class Rooms {
+    @Id
+    public String id;
+
+    public String name;
+    public Integer attendees;
+    public Boolean videoProjector;
+    public Boolean whiteboard;
+    public Boolean handicapAccess;
+
+    // Constructors
+    public Rooms() {
+    }
+
+    public Rooms(String id, String name, Integer attendees, Boolean videoProjector, Boolean whiteboard , Boolean handicapAccess) {
+        this.id = id;
+        this.name = name;
+        this.attendees = attendees;
+        this.videoProjector = videoProjector;
+        this.whiteboard = whiteboard;
+        this.handicapAccess = handicapAccess;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(Integer attendees) {
+        this.attendees = attendees;
+    }
+
+    public Boolean getVideoProjector() {
+        return videoProjector;
+    }
+
+    public void setVideoProjector(Boolean videoProjector) {
+        this.videoProjector = videoProjector;
+    }
+
+    public Boolean getWhiteboard() {
+        return whiteboard;
+    }
+
+    public void setWhiteboard(Boolean whiteboard) {
+        this.whiteboard = whiteboard;
+    }
+
+    public Boolean getHandicapAccess() {
+        return handicapAccess;
+    }
+
+    public void setHandicapAccess(Boolean handicapAccess) {
+        this.handicapAccess = handicapAccess;
+    }
+}
