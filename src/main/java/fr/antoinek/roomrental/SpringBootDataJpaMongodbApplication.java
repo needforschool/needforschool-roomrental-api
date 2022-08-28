d866bd3176a6da6b0b6811d07d8bbe7c7730c837
Antoine Kingue <etorefr@gmail.com>
2022-08-28 15:37:26 +0200

    🎨 add dotenv support on main class

--- src/main/java/fr/antoinek/roomrental/SpringBootDataJpaMongodbApplication.java
+++ src/main/java/fr/antoinek/roomrental/SpringBootDataJpaMongodbApplication.java
@@ -1,14 +1,24 @@
 package fr.antoinek.roomrental;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDataJpaMongodbApplication {
+public class SpringBootDataJpaMongodbApplication implements ApplicationRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringBootDataJpaMongodbApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDataJpaMongodbApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        logger.info("Application started");
+    }
 }
 
